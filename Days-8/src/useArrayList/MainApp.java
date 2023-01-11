a26159007b9b205577f5626643b63c98ec4efd6b
Berkay Aydoğan <berkaydogann98@gmail.com>
2023-01-11 21:57:00 +0300

    ArrayList Methods

--- Days-8/src/useArrayList/MainApp.java
+++ Days-8/src/useArrayList/MainApp.java
@@ -1,7 +1,6 @@
 package useArrayList;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -26,6 +25,57 @@ public class MainApp {
         for (String city : list) {
             System.out.println(city);
         }
+
+        //Remove items
+        //list.remove("Antalya");
+        //list.remove(0);
+        //list.removeAll(Arrays.asList("İstanbul","Ankara));
+
+        //All items clear
+        //list.clear();
+
+        //Add with index
+        list.add(0, "Berkay");
+
+        //contains -> liste için var mı
+        boolean containsStatus = list.contains("Ankara");
+        System.out.println(containsStatus);
+
+        // addAll -> Bir listenin başka listeye eklenmesi için
+        List<String> ls = new ArrayList<>();
+        ls.add("Gaziantep");
+        ls.add("Şanlıurfa");
+        ls.add("Elazığ");
+        ls.add("Gaziantep");
+        list.addAll(ls);
+
+        //indexOf -> bir değerin liste içindeki index'ini döner
+        System.out.println(list.indexOf("Antalya"));
+
+        //lastIndexOf -> var olan değerin dışında aynı değerin sondaki index'ini getirir
+        System.out.println(list.lastIndexOf("Gaziantep"));
+
+        //Var olan bir index değerini değiştirmeye yarar.
+        list.set(0, "newİstanbul");
+
+        //Sıralama
+        Collections.sort(list); // A-Z
+        Collections.reverse(list); //Z-A
+
+        //Listeyi Diziye dönüstürme
+        Object[] objects = list.toArray();
+
+        String[] arrx = {"Van", "Ağrı"};
+        List<String> newList = Arrays.asList(arrx);
+
+        //to iterator
+        System.out.println("=================");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println(list);
+
 
     }
 }
